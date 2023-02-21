d5589eaaa4985d78547548c1284cd72350de6b4b
OMINEXT\luandm <luandm@ominext.com>
2023-02-21 17:46:56 +0700

    create slider and body component

--- src/Player.js
+++ src/Player.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Body from './Body';
 import "./Player.css"
+import Sliderbar from './Sliderbar';
 export default function Player({spotify}) {
   return (
     <div className='player'>
       <div className='player__body'>
          {/*Sliderbar */}
+         <Sliderbar/>
          {/*Body */}
+         <Body/>
       </div>
    
     {/*Footer */}
---
+++ src/Sliderbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Sliderbar.css'
+export default function Sliderbar() {
+  return (
+    <div className='sliderbar'>
+    Sliderbar
+    </div>
+  );
+}
